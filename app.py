b7c95818d1ea2306a1cd3eb0d151cbf736819ad3
MariamWaell <mariammeccawi@hotmail.com>
2022-05-21 02:34:09 +0200

    first commit

--- app.py
+++ app.py
@@ -1,19 +1,37 @@
 import streamlit as st
 import numpy as np
 import pandas as pd 
+from PIL import Image
 
 
 df = pd.read_csv('diabetes.csv')
 
 
+def is_float(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+
+
 def check(text):
-        if not text.isnumeric():
-            st.warning("please enter integer")
+        if not (( text.isnumeric() or is_float(text) ) ):
+            st.error("Please fill this field with numerical value")
             
-            
-def calculate(a,b,c,d,e,f):
-   if(a.isnumeric() and b.isnumeric() and c.isnumeric() and d.isnumeric and e.isnumeric and f.isnumeric):
-       return True
+        
+        
+def check1(text):
+        if not (( text.isnumeric() or is_float(text) ) and int(text)>0):
+            return False
+        else :
+            return True
+           
+                 
+def check2(a,b,c,d,e,f):
+   if(check1(a) and check1(b) and check1 (c) and check1(d)and check1(e)and check1(f)):
+        return True
    else:
        return False
    
@@ -42,7 +60,7 @@ RandomForestClassifierModel.fit(X_train, y_train)
 
 def predict_diabetes(pregnancies, Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction, Age):
 
-    prediction=RandomForestClassifierModel.predict(np.array([[int(pregnancies),int(Glucose),int(BloodPressure),int(SkinThickness),int(Insulin),int(BMI),int(DiabetesPedigreeFunction),int(Age)]]))
+    prediction=RandomForestClassifierModel.predict(np.array([[int(float(pregnancies)),int(float(Glucose)),int(float(BloodPressure)),int(float(SkinThickness)),int(float(Insulin)),int(float(BMI)),int(float(DiabetesPedigreeFunction)),int(float(Age))]]))
     print(prediction)
     return prediction
    
@@ -54,15 +72,17 @@ def predict_diabetes(pregnancies, Glucose,BloodPressure,SkinThickness,Insulin,BM
 def main():
     # st.title("Females above 21 years old diabetes prediction form")
     html_temp = """
-    <div style="background-color:lightblue;padding:10px;border-left:6px dodgerblue">
-    <h2 style="color:white;text-align:center;">Females above 21 years old diabetes prediction form </h2>
+    <div style="background-color:#ACCFE2 ;padding:10px;border-left:100px dodgerblue">
+    <h2 style="color#:1874CD;text-align:center;font-family: Times New Roman ;">Females above 21 years old diabetes prediction form </h2>
     </div>
     """
+    
     st.markdown(html_temp,unsafe_allow_html=True)
     st.subheader("Please fill the following fields")
     
     pregnancies= st.number_input("pregnancies",min_value=0)
     Age= st.number_input("Age",min_value=21)
+    
     Glucose =st.text_input("Glucose concentration",0)
     check(Glucose)
     BloodPressure=st.text_input("Blood Pressure (mm Hg)",0)
@@ -80,22 +100,17 @@ def main():
 
     
     if st.button("Submit"):
-       if calculate(Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction ):
+       if check2(Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction ):
            result=predict_diabetes(pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age)
            if (result == 1) :
-               st.warning('You might have Diabeties. Please consult with a Doctor.')
+               st.warning('You might have Diabeties. Please consult a Doctor.')
            else:
-                st.success("Hurray! You don't have Diabeties. Please consult with Doctor for verification.")
-           #Function should be instead of done
+                st.success("Hurray! You don't have Diabeties. Please consult a Doctor for verification.")
+          
        else: 
-           st.warning("please check, all inputs must be integers")
+           st.error("Please fill the fields with the correct values")
      
-        # result=predict_note_authentication(age,gender,polyuria,polydipsia,weight,weakness,polyphagia,genital_thrush,visual_blurring,itching,irritability, delayed_healing,partial_paresis,muscle_stiffness,alopecia,obesity)
-        # if result ==1:
-        #     st.warning('You might have Diabeties. Please consult with a Doctor.')
-        # else:
-        #     st.success("Hurray! You don't have Diabeties. Please consult with Doctor for verification.")
-
+    
 if __name__=='__main__':
     main()
 
@@ -107,6 +122,16 @@ html_temp1 = """
     """
 st.markdown(html_temp1,unsafe_allow_html=True)
 
+
+page_bg_img = '''
+
+'''
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
 #sidebars
 st.sidebar.header("Diabeties Predictor Project ")
+image = Image.open('diabetess2.jpg')
+
+st.sidebar.image(image)
+
 st.sidebar.text("Team 19")
